1f6f5a34a76bcf87879ba52b01c3e5c46b53d08e
ian-lavorente <101892136+ian-lavorente@users.noreply.github.com>
2022-05-20 13:30:35 +0000

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -4,8 +4,7 @@ int main()
 {
 	int i, P = 3, Q = 5;
 
-        for (i = 1; i <= 60; i++)
-	{
+        for (i = 1; i <= 60; i++) {
 		if (i % P == 0) printf("Fizz\n");
 		else if (i % Q == 0) printf("Buzz\n");
 		else if (i % P == 0 && i % Q == 0) printf("Fizz Buzz\n");

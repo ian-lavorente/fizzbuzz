c9f6f981f002d29f99d81b1d5cc402856f28237e
ian-lavorente <101892136+ian-lavorente@users.noreply.github.com>
2022-07-21 18:03:04 +0000

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,17 +3,17 @@
 
 int main() 
 {
-	int P = 3, Q = 5;
-        for (int i = 1; i <= 60; i++) {
-		if (i % P == 0) {
-                        printf("Fizz\n");
-                } else if (i % Q == 0) {
-                        printf("Buzz\n");
-                } else if (i % P == 0 && i % Q == 0) {
-                        printf("Fizz Buzz\n");
-                } else {
-                        printf("%d\n", i);
-                }
-	}
-	return 0;
+  int P = 3, Q = 5;
+  for (int i = 1; i <= 60; i++) {
+    if (i % P == 0) {
+      printf("Fizz\n");
+    } else if (i % Q == 0) {
+      printf("Buzz\n");
+    } else if (i % P == 0 && i % Q == 0) {
+      printf("Fizz Buzz\n");
+    } else {
+      printf("%d\n", i);
+    }
+  }
+  return 0;
 }
